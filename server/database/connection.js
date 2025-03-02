89f01b25e1e90332535f76788e46b763b7ad56d5
MichaelP901 <michael.mp915@gmail.com>
2025-03-02 13:31:42 -0500

    Modified model.js and connection.js to connect to the database as well as implement database entities

---
+++ server/database/connection.js
@@ -0,0 +1,24 @@
+const {Client} = require('pg')
+
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "rootUser",
+    database: "postgres"
+})
+
+client.connect();
+
+client.query(`Select * from "Users"`, (err, res) => {
+    if(!err)
+    {
+        console.log(res.rows);
+    }
+    else
+    {
+        console.log("Error");
+        console.log(err.message);
+    }
+    client.end;
+})
